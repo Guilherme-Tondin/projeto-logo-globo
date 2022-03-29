4e9aac0e865a4658de8513e445e7093437aaaeee
Guilherme Tondin <guilhermetondin78@gmail.com>
2022-03-29 08:08:51 -0300

    commit initial

---
+++ script.js
@@ -0,0 +1,22 @@
+   {
+    
+
+    function azul(){
+
+        document.body.style.backgroundImage = "linear-gradient(80deg, rgb(3, 65, 236), rgb(73, 173, 255)"
+    }
+ 
+    function laranja(){
+        document.body.style.backgroundImage = "linear-gradient(80deg, rgb(252, 102, 65), rgb(255, 215, 103)"
+    }
+
+    function vermelho(){
+        document.body.style.backgroundImage = "linear-gradient(80deg, rgb(255, 0, 0), rgb(235, 112, 183)"
+
+    }
+
+    function none(){
+        document.body.style.backgroundImage = "none"
+
+    }
+}
